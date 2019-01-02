c8af23e8420e0f832980d216e3740370562f47c0
Konstantin Pogorelov <or@pluseq.com>
2019-01-02 12:39:39 +0100

    add transform to csv option

--- index.js
+++ index.js
@@ -15,7 +15,43 @@ app
   .option('-c, --custom [name]', 'Edit or append a custom file')
   .option('-n, --filename', 'Don\'t do anything, just output full filename')
   .option('-y, --yesterday', 'shift time in question to yesterday')
+  .option('-t, --transform', 'transform input into csv')
   .parse(process.argv)
+
+if (app.transform) {
+  const fs = require("fs")
+  const data = fs
+    .readFileSync("/dev/stdin", "utf-8")
+    .split('\n')
+    .map(
+      line => line
+        .trim()
+        .replace(/^\-\s/, '')
+        .trim()
+    )
+    .filter(line => line)
+    .forEach(line => {
+      const timeMatch = line.match(/^([^,]+),(.*)$/)
+      if (timeMatch) {
+        let time = timeMatch[1]
+        const dateMatch = time.match(/\d+\-\d+\-\d+/)
+        if (dateMatch) {
+          time = dateMatch[0]
+        }
+
+        const clean = timeMatch[2].replace(/%\w+/g, '').trim()
+        const durationMatch = clean.match(/([\d.]+)([mh])/)
+        const duration = durationMatch ?
+          (durationMatch[2] === 'm' ? (parseFloat(durationMatch[1]) / 60) : durationMatch[1])
+          : 'ERROR'
+        console.log(time + '\t' + duration + '\t' + clean.replace(/[\d.]+[mh]/, '').trim())
+      } else {
+        console.log('ERROR\tERROR\t' + line)
+      }
+    })
+
+  process.exit()
+}
 
 const mode = app.daily ? 'daily'
   : app.custom ? 'custom'
